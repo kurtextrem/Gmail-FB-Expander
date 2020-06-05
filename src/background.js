5a54aad8c0e16de0db3e593ed514eab8e1ca6b46
Jacob Groß <kurtextrem@gmail.com>
2020-06-05 09:41:22 +0200

    update for the new fb design

--- src/background.js
+++ src/background.js
@@ -1,16 +1,18 @@
 'use strict'
 
 const options = {
+	credentials: 'include',
 	cache: 'force-cache',
 	headers: new Headers({
 		Connection: 'keep-alive',
 	}),
 }
 
+/**
+ *
+ */
 function fetchCache(url) {
-	return fetch(url, options)
-		.then(checkStatus)
-		.then(toText)
+	return fetch(url, options).then(checkStatus).then(toText)
 }
 
 const commentOpen = /<!--/g,
@@ -18,21 +20,32 @@ const commentOpen = /<!--/g,
 
 let regexUsed = false
 
+/**
+ *
+ */
 function parse(text) {
-	const dom = new DOMParser().parseFromString(
-		text.replace(commentOpen, '').replace(commentClose, ''),
-		'text/html'
-	)
+	const dom = new DOMParser().parseFromString(text.replace(commentOpen, '').replace(commentClose, ''), 'text/html')
 	const element = dom.querySelector('div[data-testid="post_message"]')
+
+	let text = ''
 	if (element === null || !element.textContent) {
-		console.log(element)
-		return ''
+		const match = text.match('"story":{"message":{"text":"([^"]+)"')
+		if (match.length === 0) {
+			console.warn('no match', element)
+			return ''
+		}
+		text = match[1]
+	} else {
+		text = element.innerHTML
 	}
 
 	regexUsed = true
-	return element.innerHTML
+	return text
 }
 
+/**
+ *
+ */
 function checkStatus(response) {
 	if (response.ok) return response
 
@@ -43,19 +56,23 @@ function checkStatus(response) {
 	throw error
 }
 
+/**
+ *
+ */
 function toText(response) {
 	return response.text()
 }
 
+/**
+ *
+ */
 function error(e) {
 	console.error(e)
 	return e
 }
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	const realPath = decodeURIComponent(request.path)
-		.replace('/nd/?', '')
-		.split('&')[0]
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	const realPath = decodeURIComponent(request.path).replace('nd/?', '').split('&')[0]
 	fetchCache('https://www.facebook.com/' + realPath)
 		.then(parse)
 		.then(sendResponse)
@@ -64,6 +81,9 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	return true
 })
 
+/**
+ *
+ */
 function free() {
 	if (!regexUsed) return
 
